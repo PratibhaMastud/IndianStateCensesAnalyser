4db641887f5303713bb8f52a05834bb85c941b7d
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-23 15:30:38 +0530

    [Pratibha Mastud] Add : Test case for IndianCensesAnalyser for Incorrect Delimiter should return custom exception

--- IndianStateCensesAnalyser/IndianStateCensusAnalyserTest/TestClass.cs
+++ IndianStateCensesAnalyser/IndianStateCensusAnalyserTest/TestClass.cs
@@ -8,11 +8,12 @@ namespace IndianStateCensusAnalyserTest
 {
     public class TestClass
     {
-
         static string indianStateCensusHeaders = "﻿State,Population,AreaInSqKm,DensityPerSqKm";
+        static string indianStateCensusHeadersData = "﻿State Population AreaInSqKm DensityPerSqKm";
         static string indianStateCensusFilePath = @"C:\Users\prattii\Desktop\IndianStatesAnalyserProject\IndianStateCensesAnalyser\IndianStateCensusAnalyserTest\CsvFiles\IndianStateCensusData.csv";
         static string wrongIndianStateCensusFilePath = @"C:\Users\hp\source\repos\IndianStateDemo\CensusAnalyserTests\CSVFile\WrongIndianStateCensusData.csv";
         static string wrongIndianStateCensusFileType = @"C:\Users\prattii\Desktop\IndianStatesAnalyserProject\IndianStateCensesAnalyser\IndianStateCensusAnalyserTest\wrongIndianStateCensusFileType.txt";
+        static string delimiterIndianCensusData = @"C:\Users\prattii\Desktop\IndianStatesAnalyserProject\IndianStateCensesAnalyser\IndianStateCensusAnalyserTest\CsvFiles\DelimiterIndianCensusData.csv";
 
         CensusAnalyser censusAnalyser;
         Dictionary<string, CensusDTO> totalRecord;
@@ -52,6 +53,16 @@ namespace IndianStateCensusAnalyserTest
         {
             var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, wrongIndianStateCensusFileType, indianStateCensusHeaders));
             Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, censusException.exceptionType);
+        }
+
+        /// <summary>
+        /// Test Case 1.4 Given the indian census data csv file when correct but delimiter incoorect then return incorrect delimiter exception.
+        /// </summary>
+        [Test]
+        public void GivenIndianCensusDataFileCorrect_WhenDelimiterIncorrect_ThenShouldReturnInvalidDelimiterException()
+        {
+            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(Country.INDIA, delimiterIndianCensusData, indianStateCensusHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER, censusException.exceptionType);
         }
     }
 }
