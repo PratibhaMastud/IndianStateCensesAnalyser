af6f682d0a8900c57fa9b9a7082129dba55253bf
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-23 11:46:27 +0530

    [Pratibha Mastud] Add : By using Test Case We check Total line records in csv file

--- IndianStateCensesAnalyser/IndianStateCensesAnalyser/IndianCensusAdapter.cs
+++ IndianStateCensesAnalyser/IndianStateCensesAnalyser/IndianCensusAdapter.cs
@@ -27,10 +27,6 @@ namespace IndianStateCensesAnalyser
                     throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER, "File contains wrong delemiter");
                 }
                 string[] column = data.Split(",");
-                if (csvFilePath.Contains("IndianStateCode.csv"))
-                {
-                    dataMap.Add(column[1], new CensusDTO(new StateCodeDAO(column[0], column[1], column[2], column[3])));
-                }
                 if (csvFilePath.Contains("IndianStateCensusData.csv"))
                 {
                     dataMap.Add(column[0], new CensusDTO(new CensusDataDAO(column[0], column[1], column[2], column[3])));
---
+++ IndianStateCensesAnalyser/IndianStateCensusAnalyserTest/TestClass.cs
@@ -0,0 +1,35 @@
+﻿using IndianStateCensesAnalyser;
+using IndianStateCensesAnalyser.DTO;
+using NUnit.Framework;
+using System.Collections.Generic;
+using static IndianStateCensesAnalyser.CensusAnalyser;
+
+namespace IndianStateCensusAnalyserTest
+{
+    public class TestClass
+    {
+
+        static string indianStateCensusHeaders = "﻿State,Population,AreaInSqKm,DensityPerSqKm";
+        static string indianStateCensusFilePath = @"C:\Users\prattii\Desktop\IndianStatesAnalyserProject\IndianStateCensesAnalyser\IndianStateCensusAnalyserTest\CsvFiles\IndianStateCensusData.csv";
+
+        CensusAnalyser censusAnalyser;
+        Dictionary<string, CensusDTO> totalRecord;
+
+        [SetUp]
+        public void Setup()
+        {
+            censusAnalyser = new CensusAnalyser();
+            totalRecord = new Dictionary<string, CensusDTO>();
+        }
+
+        /// <summary>
+        /// Test Case 1.1 Given the indian census data file when reader should return census data count.
+        /// </summary>
+        [Test]
+        public void GivenIndianCensusDataFile_WhenReade_ThenShouldReturnCensusDataCount()
+        {
+            totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(29, totalRecord.Count);
+        }
+    }
+}
